ae5c6ce5658a1b8b7fe99627a34f4a03870876c8
Harrison Hou <hhw179@gmail.com>
2019-04-25 10:06:46 +1000

    try the async

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,9 @@
 import 'package:http/http.dart' as http;
 
 
-void main() {
+void main() async {
   
-  final result = http.get('http://worldclockapi.com/api/json/utc/now');
+  final result = await http.get('http://worldclockapi.com/api/json/utc/now');
   print(result.toString());
   print('main function end');
 }
